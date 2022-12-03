d1f71a1cf0bb7da7a1b16c933d2a470ebf361d3a
oluwaseunoa <oluwaseun.beicks@gmail.com>
2022-12-03 13:10:35 +0100

    Add project files.

---
+++ SwitchCaseExampleCsharp/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main() 
+    
+    {
+       //variable to store grade letter of student
+       char gradeLetter = 'A';
+
+        // find out the description based on the grade letter
+        string gradeDescription;
+
+        switch (gradeLetter)
+        {
+            case 'A': gradeDescription = "Outstanding"; break;
+            case 'B': gradeDescription = "Excelent"; break;
+            case 'C': gradeDescription = "Good"; break;
+            case 'D': gradeDescription = "Average"; break;
+            case 'F': gradeDescription = "Fail"; break;
+            default: gradeDescription = "None"; break;
+        }
+        System.Console.WriteLine(gradeDescription);
+        System.Console.ReadKey();   
+    }
+}
